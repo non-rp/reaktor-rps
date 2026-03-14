961accd53d4ca85d3d8956272e18a6cc35201f8e
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-03-14 20:16:04 +0200

    implement front-end polling cached latest matches

--- web/src/components/common/UserProfileLink.tsx
+++ web/src/components/common/UserProfileLink.tsx
@@ -1,12 +1,16 @@
 import { Link } from '@mui/material'
 
 type UserProfileLinkProps = {
-  id: number
+  id: number | null
   name: string
   onNavigate: (path: string) => void
 }
 
 export function UserProfileLink({ id, name, onNavigate }: UserProfileLinkProps) {
+  if (id === null) {
+    return <>{name}</>
+  }
+
   const href = `/users/${id}`
 
   return (
